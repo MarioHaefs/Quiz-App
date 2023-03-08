ef8066201e5be275e453f9b3e77dbfddc2751485
Mario Haefs <MarioHaefs@t-online.de>
2023-03-08 12:59:24 +0100

    added Start Page

---
+++ script.js
@@ -0,0 +1,48 @@
+let kulinarik =  [
+    {
+        "question": "Welches Gemüse ist der Hauptdarsteller eines bekannten italienischen Desserts in Verbindung mit dunkler Schokolade?",
+        "answer_1": "Paprika",
+        "answer_2": "Zucchini",
+        "answer_3": "Aubergine",
+        "answer_4": "Spinat",
+        "right_answer": 3
+    },
+    {
+        "question": "Für welches Sandwich ist die Stadt Philadelphia weltweit bekannt?",
+        "answer_1": "Cheesesteak Sandwich",
+        "answer_2": "Pastrami Sandwich",
+        "answer_3": "Club Sandwich",
+        "answer_4": "Italian Beef Sandwich",
+        "right_answer": 1
+    },
+    {
+        "question": "Ein Ausflug nach Schottland: Wie nennt man den mit Herz, Leber, Lunge, Nierenfett vom Schaf, Zwiebeln und Hafermehl gefüllten Schafsmagen?",
+        "answer_1": "Saumagen",
+        "answer_2": "Porridge",
+        "answer_3": "Baps",
+        "answer_4": "Haggis",
+        "right_answer": 4
+    },
+    {
+        "question": "Kimchi gilt als Superfood der koreanischen Küche. Was ist Kimchi?",
+        "answer_1": "Gegorener Fisch",
+        "answer_2": "Gebratener Seidentofu",
+        "answer_3": "Gegorenes Gemüse",
+        "answer_4": "Fermentierte Sprossen",
+        "right_answer": 3
+    },
+    {
+        "question": "Pommes frites, Cheese curds darauf, mit Bratensauce übergiessen - fertig ist ein beliebtes Fastfood-Gericht Kanadas? Wie heisst es?",
+        "answer_1": "Trumpone",
+        "answer_2": "Poutine",
+        "answer_3": "Chipotle",
+        "answer_4": "Pastore",
+        "right_answer": 2
+    },
+]
+
+
+function startQuiz() {
+    let content = document.getElementById('content');
+    
+}
