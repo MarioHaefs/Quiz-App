345897498a18a0f4dbdf46337a6fa27d6a9f2b7c
Mario Haefs <MarioHaefs@t-online.de>
2023-03-13 11:30:54 +0100

    added Pointer Events functions

--- script.js
+++ script.js
@@ -44,6 +44,8 @@ let kulinarik = [
 let currentQuestion = 0;
 let rightQuestions = 0;
 let number = 1;
+let AUDIO_SUCCESS = new Audio('audio/success.mp3');
+let AUDIO_FAIL = new Audio('audio/fail.mp3');
 
 
 
@@ -97,24 +99,31 @@ function changeInnerContent() {
 }
 
 
-// Zeigt die Fragen aus unserem JSON in unserem Quiz an. Zusätzlich aktualisiert sich die Progress-Bar //
+// Zeigt die Fragen aus unserem JSON in unserem Quiz an //
 function showQuestion() {
     let question = kulinarik[currentQuestion];
-    let percent = number / kulinarik.length;
-    percent = percent * 100;
-
-    document.getElementById('progress-bar').innerHTML = `${percent} %`;
-    document.getElementById('progress-bar').style = `width: ${percent}%;`;
-
+    
     document.getElementById('question-text').innerHTML = question['question'];
     document.getElementById('answer-1').innerHTML = question['answer_1'];
     document.getElementById('answer-2').innerHTML = question['answer_2'];
     document.getElementById('answer-3').innerHTML = question['answer_3'];
     document.getElementById('answer-4').innerHTML = question['answer_4'];
+
+    updateProgressBar();
 }
 
 
-// Unterscheidet richtige und falsche Antwort und markiert dementsprechend farblich //
+// Updatet bei jeder beantworteten Frage die Progress Bar um in diesem Fall 20% //
+function updateProgressBar() {
+    let percent = number / kulinarik.length;
+    percent = percent * 100;
+
+    document.getElementById('progress-bar').innerHTML = `${percent} %`;
+    document.getElementById('progress-bar').style = `width: ${percent}%;`;
+}
+
+
+// Unterscheidet richtige und falsche Antwort, markiert dementsprechend farblich und spielt einen jeweiligen Sound ab //
 function answer(selection) {
     let question = kulinarik[currentQuestion];
     let selectedQuestionNumber = selection.slice(-1);
@@ -122,15 +131,34 @@ function answer(selection) {
 
     if (selectedQuestionNumber == question['right_answer']) {
         document.getElementById(selection).classList.add('bg-lightgreen');
+        AUDIO_SUCCESS.play();
         rightQuestions++;
     } else {
         document.getElementById(selection).classList.add('bg-lightred');
         document.getElementById(idOfRightAnswer).classList.add('bg-lightgreen');
+        AUDIO_FAIL.play();
     }
+    disablePointerEvents();
 }
 
 
-// Zeigt bei onclick auf die Pfeile die voherige oder nächste Frage an //
+function disablePointerEvents() {
+    document.getElementById('answer_1').style = 'pointer-events:none;';
+    document.getElementById('answer_2').style = 'pointer-events:none;';
+    document.getElementById('answer_3').style = 'pointer-events:none;';
+    document.getElementById('answer_4').style = 'pointer-events:none;';
+}
+
+
+function enablePointerEvents() {
+    document.getElementById('answer_1').style = 'pointer-events:auto;';
+    document.getElementById('answer_2').style = 'pointer-events:auto;';
+    document.getElementById('answer_3').style = 'pointer-events:auto;';
+    document.getElementById('answer_4').style = 'pointer-events:auto;';
+}
+
+
+// Zeigt bei onclick auf die Pfeile die vorherige oder nächste Frage an //
 function nextQuestion() {
     if (currentQuestion >= 4) {
         showEndScreen();
@@ -141,6 +169,7 @@ function nextQuestion() {
         showQuestion();
     }
     resetAnswerButtons();
+    enablePointerEvents();
 }
 
 
@@ -154,6 +183,7 @@ function previousQuestion() {
         showQuestion();
     }
     resetAnswerButtons();
+    enablePointerEvents();
 }
 
 
@@ -178,7 +208,10 @@ function showEndScreen() {
     content.innerHTML += endScreenHTML();
     
     let hideArrow = document.getElementById('arrows')
+    let showTrophy = document.getElementById('trophy')
     hideArrow.classList.add("hide");
+    showTrophy.classList.remove('hide');
+
     endResult();
 }
 
@@ -187,7 +220,7 @@ function endScreenHTML() {
     return  /*html*/`
     <div class="end-screen">
         <img class="brain-img" src="img/brain result.png">
-        <span class="endScreen-text">Herzlichen Glückwunsch! Du hast das Quiz abgeschlossen!</span>
+        <span class="endScreen-text">Du hast das Quiz abgeschlossen!</span>
         <span class="endScreen-text"><p class="color-orangered">Dein Ergebnis:</p><p id="result"></p></span>
         <button type="button" class="btn btn-primary share-button">SHARE</button>
         <button onclick="replay()" class="replay-button" type="button">REPLAY</button>
@@ -204,5 +237,10 @@ function  endResult(){
 
 // Lädt das Quiz neu und du kannst nochmal spielen //
 function replay() {
-    window.location.href = 'index.html';
+    let showTrophy = document.getElementById('trophy')
+    showTrophy.classList.add('hide');
+    currentQuestion = 0;
+    rightQuestions = 0;
+    number = 1;
+    startQuiz();
 }
